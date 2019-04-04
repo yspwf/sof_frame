f0655a0680c985855d054879cb5076ecaffa2b95
ysp123 <846163966@qq.com>
2019-04-04 18:40:26 +0800

    request

--- src/request.php
+++ src/request.php
@@ -13,20 +13,20 @@ class request{
         if($url == '/favicon.ico'){
             return false;
         }
-        
+        echo $url;
         return $this->context($url);
     }
 
 
-    public function context(){
+    public function context($url){
         $keys = [];
-        array_walk($query, function($value, $key) use(&$keys){
+        array_walk($url, function($value, $key) use(&$keys){
                 if($key%2==0){
                     $keys[] = $value;
                 }
         });
         $values = [];
-        array_walk($query, function($value, $key) use(&$values){
+        array_walk($url, function($value, $key) use(&$values){
                 if($key%2!=0){
                     $values[] = htmlspecialchars($value);
                 }
