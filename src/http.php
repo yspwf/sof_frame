1de3a2dce2485eff1302b4c1ee2f3d510c462af7
ysp123 <846163966@qq.com>
2019-04-04 17:57:37 +0800

    autoload

--- src/http.php
+++ src/http.php
@@ -35,7 +35,7 @@ class http{
 
                $pathinfo = $request->server['path_info'];
                echo $pathinfo;
-               new route($pathinfo);
+               new \home\user();
                $response->end('hello swoole');
     }
 
--- src/register.php
+++ src/register.php
@@ -6,17 +6,36 @@ class register{
     //     spl_autoload_register([$this, 'autoload']);
     // }
 
-    public static function autoload($classname){
-        echo "23423424234";
-        echo $classname;
-       // $classname = str_replace('\\','/', $classname);
-        $basePath = str_replace('\\', DIRECTORY_SEPARATOR, ROOT.DIRECTORY_SEPARATOR.$classname);
-        $file = $basePath.".php";
-        echo $file;
-        if(file_exists($file)){
-            require_once $file;
+    // public static function autoload($classname){
+    //     echo "23423424234";
+    //     echo $classname;
+    //    // $classname = str_replace('\\','/', $classname);
+    //     $basePath = str_replace('\\', DIRECTORY_SEPARATOR, ROOT.DIRECTORY_SEPARATOR.$classname);
+    //     $file = $basePath.".php";
+    //     echo $file;
+    //     if(file_exists($file)){
+    //         require_once $file;
+    //     }
+    // }
+
+ 
+
+        /**
+         * 自动加载
+         */
+        public static function autoload($class){
+            echo "234234234234234234234";
+            $basePath = str_replace('\\', DIRECTORY_SEPARATOR, ROOT.DIRECTORY_SEPARATOR.$class);
+            $file = $basePath.'.php';
+            echo $file;
+            if(file_exists($file)){
+                require_once $file;
+            }else{
+                throw new \Exception('No file find for this path!');
+            }
         }
-    }
+    
+    
 
 }
 
