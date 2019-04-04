2ee9c63b72e1ed838364ce18e06cb46cf3b6427b
ysp123 <846163966@qq.com>
2019-04-04 19:39:28 +0800

    db

--- src/db/connect.php
+++ src/db/connect.php
@@ -2,9 +2,46 @@
 namespace sof\db;
 class connect{
 
-    private $instance;
+    private static $instance;
 
-    
+    private $channel;
+
+    public function getInstance(){
+        if(!(self::$instance instanceof self)){
+            $self = new self();
+            $self->connect();
+            self::$instance = $self;
+        }
+        return self::$instance;
+    }
+
+
+    public function connect(){
+        $this->channel = new \Swoole\Coroutine\Channel(10);
+        for($i=0;$i<10;$i++){
+            $mysql = new \Swoole\Coroutine\MySQL();
+            $mysql->connect([
+                'host'=>'127.0.0.1',
+                'port'=>3306,
+                'user'=>'root',
+                'password'=>'',
+                'database'=>'demo'
+            ]);
+            $this->push($mysql);
+        }
+    }
+
+    public function push($mysql){
+        $this->channel->push($mysql);
+    }
+
+    public function length(){
+        return $this->channel->length();
+    }
+
+    public function get(){
+        return $this->channel->pop();
+    }
 
 }
 
