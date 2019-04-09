57e5f9817bfe96e0517f9b21cefb8f3b1a8a2679
ysp123 <846163966@qq.com>
2019-04-09 10:50:36 +0800

    home

--- home/index.php
+++ home/index.php
@@ -5,8 +5,8 @@ class index extends controller{
 
     public function index(){
        // echo "welcome swoole http server".PHP_EOL;
-       $this->write(['hh'=>'哈哈哈哈','gg'=>'坎坎坷坷扩']);
-       //return ['name'=>'ysp','sex'=>'男'];
+       //$this->write(['hh'=>'哈哈哈哈','gg'=>'坎坎坷坷扩']);
+       return ['name'=>'ysp','sex'=>'男'];
     }
 
 }
