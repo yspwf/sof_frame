7f4e56b3776c1f8e4a43282da603fa4d7e661152
ysp123 <846163966@qq.com>
2019-04-11 10:15:56 +0800

    db

--- src/db/connect.php
+++ src/db/connect.php
@@ -40,8 +40,8 @@ class connect{
 
 
     public function connect(){
-        $this->channel = new \Swoole\Coroutine\Channel(100);
-        for($i=0;$i<100;$i++){
+        $this->channel = new \Swoole\Coroutine\Channel(500);
+        for($i=0;$i<500;$i++){
             $mysql = new \Swoole\Coroutine\MySQL();
             $status = $mysql->connect([
                 'host'=>'127.0.0.1',
